8cfd5a72bff92afa1023165b2875371814c66010
Heesu0312 <143991712+Heesu0312@users.noreply.github.com>
2023-09-18 01:56:46 +0900

    w2room

--- src/w2/assignment/basic1/script/sketch.js
+++ src/w2/assignment/basic1/script/sketch.js
@@ -1,16 +1,54 @@
 function setup() {
   setCanvasContainer('canvas', 3, 2, true);
-  ellipseMode(CENTER);
+  background('white');
 }
 
 function draw() {
-  background('orange');
+  background('#B4CDCD');
 
   noStroke();
-  fill('blue');
-  circle(80, 80, 70);
-  fill('red');
-  rect(100, 100, 20, 20);
-  fill('yellow');
-  quad(20, 30, 80, 30, 80, 70, 20, 70);
+  colorMode(RGB);
+
+  fill('#7A8B8B');
+  rect(450, 70, 300, 350);
+  fill('#BFEFFF');
+  rect(470, 90, 120, 300);
+  fill('#BFEFFF');
+  rect(610, 90, 120, 300);
+  fill('#9AC0CD');
+  rect(0, 440, 900, 100);
+  fill('white');
+  rect(400, 350, 400, 80);
+  fill('#68838B');
+  rect(400, 420, 400, 20);
+  fill('#68838B');
+  rect(780, 270, 20, 150);
+  fill('#607B8B');
+  rect(300, 310, 25, 130);
+  fill('#607B8B');
+  rect(10, 310, 25, 130);
+  fill('#607B8B');
+  rect(10, 310, 310, 80);
+  fill('#6E7B8B');
+  rect(70, 180, 150, 100);
+  fill('#A2B5CD');
+  rect(85, 190, 120, 78);
+  fill('#6E7B8B');
+  rect(120, 280, 50, 30);
+  fill('#6E7B8B');
+  rect(95, 300, 100, 10);
+  fill('#6E7B8B');
+  rect(230, 300, 50, 10);
+  fill('#7A8B8B');
+  rect(120, 420, 100, 20);
+  fill('#8DB6CD');
+  rect(400, 330, 270, 20);
+  fill('#8DB6CD');
+  rect(730, 330, 50, 20);
+  fill('#68838B');
+  rect(375, 0, 10, 130);
+  fill('orange');
+  circle(380, 100, 70);
+  fill('white');
+  circle(380, 90, 50);
 }
