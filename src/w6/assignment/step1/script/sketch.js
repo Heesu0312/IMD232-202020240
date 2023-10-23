aa459c2ed38e35a4934147ac8eaa83fb04c9cc0b
Heesu0312 <143991712+Heesu0312@users.noreply.github.com>
2023-10-23 15:40:06 +0900

    w6step final

--- src/w6/assignment/step1/script/sketch.js
+++ src/w6/assignment/step1/script/sketch.js
@@ -12,26 +12,22 @@ function setup() {
 function draw() {
   background(360, 0, 100);
 
-  // 파티클 생성
   if (random(1) < 0.5) {
     let x = random(width);
     let p = new Particle(x, 0);
     particles.push(p);
   }
 
-  // 파티클 업데이트 및 그리기
   for (let i = particles.length - 1; i >= 0; i--) {
     let p = particles[i];
-    p.applyForce(gravity); // 중력 적용
+    // p.applyForce(gravity);
     p.update();
     p.display();
 
-    // 화면을 벗어난 파티클 제거
     if (p.isOffScreen()) {
       particles.splice(i, 1);
     }
   }
 
-  // 현재 파티클의 갯수를 콘솔에 표시
   console.log('현재 파티클 수: ' + particles.length);
 }
