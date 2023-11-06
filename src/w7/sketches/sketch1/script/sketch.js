31341e80326e9cb6ef27cc5930246877e5cdf45e
Heesu0312 <143991712+Heesu0312@users.noreply.github.com>
2023-11-06 15:02:51 +0900

    class7

--- src/w7/sketches/sketch1/script/sketch.js
+++ src/w7/sketches/sketch1/script/sketch.js
@@ -1,111 +1,110 @@
-class Emitter {
-  constructor(emittingPosX, emittingPosY) {
-    this.emittingPos=createVector(emittingPosX, y);
-    this.balls = [];
-  }
+var Engine = Matter.Engine,
+  Render = Matter.Render,
+  Runner = Matter.Runner,
+  Composites = Matter.Composites,
+  Events = Matter.Events,
+  Constraint = Matter.Constraint,
+  MouseConstraint = Matter.MouseConstraint,
+  Mouse = Matter.Mouse,
+  Body = Matter.Body,
+  Composite = Matter.Composite,
+  Bodies = Matter.Bodies;
 
-  createBall() {
-    this.balls.push(
-      new ball(
-        this.emittingPos.x, 
-        this.emittingPos.y, 
-        random(1, 5)
-        random(360),
-        100,
-        50
-      )
-    );
-  }
+// create engine
+var engine = Engine.create(),
+  world = engine.world;
 
-  applyForce(force) {
-    this.balls.forEach((each)=> {
-      each.applyForce(force);
-    })
-  }
+// create renderer
+var render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    width: 800,
+    height: 600,
+    showAngleIndicator: true,
+  },
+});
 
-  update() {
-    this.balls.forEach((each) => {
-      each.update();
-    })
-  }
+Render.run(render);
 
-  display() {
-    this.balls.forEach((each) => {
-      each.update();
-    })
-  }
+// create runner
+var runner = Runner.create();
+Runner.run(runner, engine);
 
-class ball {
-  constructor(posX, posY, mass, h, s, v) {
-    this.pos = createVector(posX, posY);
-    
-    this.vel = createVector();
-    
-    this.acc = createVector();
-    
-    this.mass = mass;
-    
-    this.rad = this.mass * 5;
-    
-    this.color = color(h, s, v);
-  }
- 
-  applyForce(force) {
-    const calcedAcc = p5.Vector.div(force, this.mass);
-    
-    this.acc.add(calcedAcc);
-  }
-
-  update() {
-    this.vel.add(this.acc);
-    
-    this.pos.add(this.vel);
-    
-    this.acc.mult(0);
-    
-  }
-  display() {
-    fill(this.color);
-    noStroke();
-    ellipse(this.pos.x, this.pos.y, 2 * this.rad);
-  }
-}
-
-let emitter;
-let balls = [];
-let gravity;
-let wind;
-
-function setup() {
-  setCanvasContainer('mySketchGoesHere', 2, 1, true);
-
-  colorMode(HSL, 360, 100, 100);
-
-  emitter=new Emitter(width/2,0);
-  
-  for (let n = 0; n < 10; n++) {
-    balls.push(new ball(random(width), 0, random(1, 20), random(360), 100, 50));
-  }
-
-  gravity = createVector(0, 0.1);
-  wind = createVector(0.5, 0);
-
-  background(255);
-}
-function draw() {
-  background(255);
-  balls.forEach((each) => {
-    const scaledG = p5.Vector.mult(gravity, each.mass);
-    each.applyForce(scaledG);
-    each.applyForce(wind);
-    each.update();
-    each.display();
+// add bodies
+var ground = Bodies.rectangle(395, 600, 815, 50, {
+    isStatic: true,
+    render: { fillStyle: '#060a19' },
+  }),
+  rockOptions = { density: 0.004 },
+  rock = Bodies.polygon(170, 450, 8, 20, rockOptions),
+  anchor = { x: 170, y: 450 },
+  elastic = Constraint.create({
+    pointA: anchor,
+    bodyB: rock,
+    length: 0.01,
+    damping: 0.01,
+    stiffness: 0.05,
   });
-}
 
-function mousePressed() {
-  for (let n = 0; n < balls.length; n++) {
-    balls[n] = new ball(random(width), 0, random(1, 20), random(360), 100, 50);
+var pyramid = Composites.pyramid(500, 300, 9, 10, 0, 0, function (x, y) {
+  return Bodies.rectangle(x, y, 25, 40);
+});
+
+var ground2 = Bodies.rectangle(610, 250, 200, 20, {
+  isStatic: true,
+  render: { fillStyle: '#060a19' },
+});
+
+var pyramid2 = Composites.pyramid(550, 0, 5, 10, 0, 0, function (x, y) {
+  return Bodies.rectangle(x, y, 25, 40);
+});
+
+Composite.add(engine.world, [
+  ground,
+  pyramid,
+  ground2,
+  pyramid2,
+  rock,
+  elastic,
+]);
+
+Events.on(engine, 'afterUpdate', function () {
+  if (
+    mouseConstraint.mouse.button === -1 &&
+    (rock.position.x > 190 || rock.position.y < 430)
+  ) {
+    // Limit maximum speed of current rock.
+    if (Body.getSpeed(rock) > 45) {
+      Body.setSpeed(rock, 45);
+    }
+
+    // Release current rock and add a new one.
+    rock = Bodies.polygon(170, 450, 7, 20, rockOptions);
+    Composite.add(engine.world, rock);
+    elastic.bodyB = rock;
   }
-}
-}
+});
+
+// add mouse control
+var mouse = Mouse.create(render.canvas),
+  mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+      stiffness: 0.2,
+      render: {
+        visible: false,
+      },
+    },
+  });
+
+Composite.add(world, mouseConstraint);
+
+// keep the mouse in sync with rendering
+render.mouse = mouse;
+
+// fit the render viewport to the scene
+Render.lookAt(render, {
+  min: { x: 0, y: 0 },
+  max: { x: 800, y: 600 },
+});
