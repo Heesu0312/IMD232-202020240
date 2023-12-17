d95893728e0c0cfaa4e6ce54825df72acd40661e
Heesu0312 <143991712+Heesu0312@users.noreply.github.com>
2023-12-18 01:11:56 +0900

    ...

--- src/final/sketches/sketch2/script/sketch.js
+++ src/final/sketches/sketch2/script/sketch.js
@@ -1,23 +1,71 @@
-let aDrunkenObj;
-let trace = [];
+let dandelionSeeds = [];
 
 function setup() {
-  setCanvasContainer('canvas', 1, 1, true);
-
-  aDrunkenObj = new aDrunkenObj(width / 2, height / 2);
-
-  background('white');
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 50; i++) {
+    dandelionSeeds.push(new DandelionSeed(random(width), random(height)));
+  }
 }
 
 function draw() {
-  const randomForce = p5.Vector.random2D();
-  randomForce.mult(1);
-  aDrunkenObj.applyForce(randomForce);
-  aDrunkenObj.update();
-  aDrunkenObj.infiniteEdge();
+  background('#87CEEB');
 
-  trace.push([aDrunkenObj.pos.x, aDrunkenObj.pos.y]);
+  for (let seed of dandelionSeeds) {
+    let distance = dist(mouseX, mouseY, seed.x, seed.y);
+    let force = 100 / (distance + 1);
 
-  background('white');
-  aDrunkenObj.display();
+    seed.move(force);
+    seed.display();
+  }
+}
+
+class DandelionSeed {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = random(2, 8);
+    this.color = color(255, 255, 255);
+    this.triangleOpacity = 70;
+    this.speed = random(0.5, 2);
+    this.angle = random(TWO_PI);
+  }
+
+  move(force) {
+    let angleToMouse = atan2(mouseY - this.y, mouseX - this.x);
+    this.x += cos(angleToMouse) * force * this.speed;
+    this.y += sin(angleToMouse) * force * this.speed;
+
+    this.x = constrain(this.x, 0, width);
+    this.y = constrain(this.y, 0, height);
+
+    this.angle += random(-0.1, 0.1);
+    this.x += cos(this.angle) * this.speed;
+    this.y += sin(this.angle) * this.speed;
+
+    if (this.x < 0 || this.x > width || this.y < 0 || this.y > height) {
+      this.x = random(width);
+      this.y = random(height);
+    }
+  }
+
+  display() {
+    stroke(255);
+    strokeWeight(1);
+    line(this.x, this.y - this.size / 2, this.x, this.y - this.size);
+
+    fill(this.color);
+    noStroke();
+    ellipse(this.x, this.y, this.size, this.size);
+
+    fill(255, this.triangleOpacity);
+    noStroke();
+    triangle(
+      this.x - (cos(PI / 3) * this.size) / 2,
+      this.y - this.size,
+      this.x,
+      this.y - this.size - 5,
+      this.x + (cos(PI / 3) * this.size) / 2,
+      this.y - this.size
+    );
+  }
 }
--- src/final/sketches/sketch3/script/sketch.js
+++ src/final/sketches/sketch3/script/sketch.js
@@ -1,23 +1,60 @@
-let aDrunkenObj;
-let trace = [];
+let marshmallows = [];
 
 function setup() {
-  setCanvasContainer('canvas', 1, 1, true);
-
-  aDrunkenObj = new aDrunkenObj(width / 2, height / 2);
-
-  background('white');
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  const randomForce = p5.Vector.random2D();
-  randomForce.mult(1);
-  aDrunkenObj.applyForce(randomForce);
-  aDrunkenObj.update();
-  aDrunkenObj.infiniteEdge();
+  background('#87CEEB');
 
-  trace.push([aDrunkenObj.pos.x, aDrunkenObj.pos.y]);
+  for (let i = marshmallows.length - 1; i >= 0; i--) {
+    marshmallows[i].update();
+    marshmallows[i].display();
 
-  background('white');
-  aDrunkenObj.display();
+    if (marshmallows[i].isOffscreen()) {
+      marshmallows.splice(i, 1);
+    }
+  }
+}
+
+function mousePressed() {
+  for (let i = 0; i < 5; i++) {
+    marshmallows.push(new Marshmallow(mouseX, mouseY));
+  }
+}
+
+class Marshmallow {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = random(20, 40);
+    this.color = color(255, 228, 196, 90);
+    this.xSpeed = random(-2, 2);
+    this.ySpeed = random(-2, 2);
+  }
+
+  update() {
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+
+    let angle = atan2(mouseY - this.y, mouseX - this.x);
+    this.x += cos(angle) * 2;
+    this.y += sin(angle) * 2;
+  }
+
+  display() {
+    noStroke();
+    fill(this.color);
+    ellipse(this.x, this.y, this.size, this.size);
+  }
+
+  isOffscreen() {
+    return (
+      this.x < 0 ||
+      this.x > width ||
+      this.y < 0 ||
+      this.y > height ||
+      this.size < 0
+    );
+  }
 }
